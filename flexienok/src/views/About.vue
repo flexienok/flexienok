f7ace165c5efa1ca3eb084872c7b32d676c7418f
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-13 18:35:50 +0100

    add about

--- flexienok/src/views/About.vue
+++ flexienok/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<v-flex xs12 align="center"
+          justify="center">
+  
+
+      <v-row  
+          align="center"
+          justify="center"
+        >
+    <h1 id="Head">This is an about page</h1>
+  </v-row>
+  <v-row
+            align="center"
+          justify="center"
+          >
+
+        <span id="Body">
+      Nokiflexi
+    </span>
+  </v-row>
+
+</v-flex>
 </template>
+
+
+<style scoped>
+#Body {
+  color: rgb(255, 255, 255);
+}
+
+#Head {
+  color: rgb(255, 255, 255);
+}
+</style>
--- flexienok/src/views/Home.vue
+++ flexienok/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
+        <v-row  
+          align="center"
+          justify="center"
+        >
     <img alt="Vue logo" src="../assets/logo.png">
-    
+    </v-row>
   </div>
 </template>
+
