eac29c9bed6402a2f510e748f8650883bd277e3e
tokfrans03 <34898241+tokfrans03@users.noreply.github.com>
2019-11-16 19:57:53 +0100

    fixed deploy.sh and some cleanup

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,18 @@
 set -e
 
 # build
-cd flexienok
+cd flexienok/
+echo "##############################"
+echo
+echo "installing..."
+echo
+echo "##############################"
 npm i
+echo "##############################"
+echo
+echo "Building..."
+echo
+echo "##############################"
 npm run build
 
 # navigate into the build output directory
@@ -18,10 +28,16 @@ git init
 git add -A
 git commit -m 'deploy'
 
+echo "##############################"
+echo
+echo "Commiting..."
+echo
+echo "##############################"
+
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:flexienok/flexienok.github.io.git master
+git push -f git@github.com:flexienok/flexienok.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:flexienok/flexienok.github.io.git master:master
+# git push -f git@github.com:flexienok/flexienok.github.io.git master:master
 
 cd -
